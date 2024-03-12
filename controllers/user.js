2c641e76de19da66237f879f37970a36bd6767fb
Miad Hasan <mdhasan9.sy@gmail.com>
2024-03-12 19:34:48 +0600

    implemented: update route

--- controllers/user.js
+++ controllers/user.js
@@ -30,3 +30,25 @@ export const deleteUser = async (req, res) => {
     res.status(404).json(err);
   }
 };
+
+export const updateUser = async (req, res) => {
+  const userId = req.params.id;
+  const requestedUpdates = Object.keys(req.body);
+  const allowedUpdates = ["firstName", "lastName", "phone", "isBlocked"];
+  const isValidUpdate = requestedUpdates.every((item) =>
+    allowedUpdates.includes(item),
+  );
+
+  if (!isValidUpdate) {
+    return res.status(400).send("Invalid updates!");
+  }
+  const user = req.body;
+  try {
+    const updatedUser = await User.findByIdAndUpdate(userId, user, {
+      new: true,
+    });
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(404).json(err);
+  }
+};
--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,8 @@ import express from "express";
 import {
   createUser,
   getUsers,
-  deleteUser
+  deleteUser,
+  updateUser
 } from "../controllers/user.js";
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.post("/", createUser);
 router.get("/", getUsers);
 router.delete("/:id", deleteUser);
+router.patch("/:id", updateUser);
 
 export default router;
