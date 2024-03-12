b6a074051db83d1705579109db5de4aa59a4d40b
Miad Hasan <mdhasan9.sy@gmail.com>
2024-03-12 17:57:24 +0600

    implemented: get user route

--- controllers/user.js
+++ controllers/user.js
@@ -10,3 +10,12 @@ export const createUser = async (req, res) => {
     res.status(409).json(err);
   }
 };
+
+export const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(404).json(err);
+  }
+};
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,13 @@
 import express from "express";
 import {
-  createUser
+  createUser,
+  getUsers
 } from "../controllers/user.js";
 
 const router = express.Router();
 
 router.post("/", createUser);
+router.get("/", getUsers);
+
 
 export default router;
